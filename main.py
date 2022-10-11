73ffaca5a7aa6dd6f430e9760a471e6f1ffbb236
Alex <aboyle@zap.me>
2022-10-11 21:17:42 +1300

    Start on printing date - count for each line in input file

--- main.py
+++ main.py
@@ -11,10 +11,22 @@ def count_all(file_):
         total += int(line_array[len(line_array) - 1])
     return total
 
+def print_dates(file_):
+    # prints all dates + count | void
+    for line in file_:
+        line_array_date = line.split('T')
+        date = line_array_date[0]
+        line_array_count = line.split()
+        count = int(line_array_count[len(line_array_count) -1])
+        print("{0} {1}".format(date, count))
+
+
 def main():
     # main function to run | void
     with open("input.txt", "r") as f:
         total = count_all(f)
         print("total is {0}".format(total))
+    with open("input.txt", "r") as f:
+        print_dates(f)
 main()
 
